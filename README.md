55292afaf1c3ccef96909a3bb420d73656194a02
ARME <alessioarm69@gmail.com>
2025-07-19 16:05:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Description
+Enable, disable or reload any installed plugin via the command palette. It can also disable all plugins and remember which ones were active to re-enable them with the another command later
+# Installation
+1.  Download the latest release from GitHub.
+2.  Unzip the contents into your vault's `.obsidian/plugins/plugins-toggler` directory.
+3.  Enable the plugin in Obsidian's settings.
+# Usage
+1.  Open the command palette (Ctrl/Cmd + P).
+2.  Search for "Plugins Toggler: Enable [Plugin Name]" or "Plugins Toggler: Disable [Plugin Name]".
+3.  Select the desired command to toggle the plugin.
+
+**Note on "Disable ALL plugins (remembering active ones)":** When this command is used, the list of currently active plugins is saved. If you use this command again, the previously saved list will be overwritten with the new set of active plugins
+# Installation
+## Manual
+## From community plugins tab
+# Disclaimer
---
+++ main.ts
@@ -0,0 +1,149 @@
+import { App, Plugin, PluginManifest, PluginSettingTab, Setting, Notice } from 'obsidian';
+
+interface PluginsTogglerSettings {
+    lastActivePlugins: string[];
+    disableSelf: boolean;
+}
+
+const DEFAULT_SETTINGS: PluginsTogglerSettings = {
+    lastActivePlugins: [],
+    disableSelf: true,
+}
+
+export default class PluginsToggler extends Plugin {
+    settings!: PluginsTogglerSettings;
+
+    async onload() {
+        await this.loadSettings();
+        this.addSettingTab(new PluginsTogglerSettingTab(this.app, this));
+
+        // Using onLayoutReady ensures that all plugins are loaded and their manifests are available.
+        this.app.workspace.onLayoutReady(() => {
+            this.registerPluginCommands();
+        });
+    }
+
+    registerPluginCommands() {
+        this.addGlobalCommands();
+
+        // @ts-ignore
+        const manifests = this.app.plugins.manifests;
+
+        for (const pluginId in manifests) {
+            // Don't add commands for this plugin itself.
+            if (pluginId === this.manifest.id) {
+                continue;
+            }
+
+            const manifest = manifests[pluginId];
+            const pluginName = manifest.name;
+
+            // ADD ENABLE COMMAND
+            this.addCommand({
+                id: `plugins-toggler-enable-${pluginId}`,
+                name: `Enable: ${pluginName}`,
+                callback: async () => {
+                    // @ts-ignore
+                    await this.app.plugins.enablePlugin(pluginId);
+                    new Notice(`Plugin enabled: ${pluginName}`);
+                }
+            });
+
+            // ADD DISABLE COMMAND
+            this.addCommand({
+                id: `plugins-toggler-disable-${pluginId}`,
+                name: `Disable: ${pluginName}`,
+                callback: async () => {
+                    // @ts-ignore
+                    await this.app.plugins.disablePlugin(pluginId);
+                    new Notice(`Plugin disabled: ${pluginName}`);
+                }
+            });
+
+            // ADD RELOAD COMMAND
+            this.addCommand({
+                id: `plugins-toggler-reload-${pluginId}`,
+                name: `Reload: ${pluginName}`,
+                callback: async () => {
+                    // @ts-ignore
+                    await this.app.plugins.disablePlugin(pluginId);
+                    // @ts-ignore
+                    await this.app.plugins.enablePlugin(pluginId);
+                    new Notice(`Plugin reloaded: ${pluginName}`);
+                }
+            });
+        }
+    }
+
+    addGlobalCommands() {
+        this.addCommand({
+            id: 'plugins-toggler-disable-all-remember',
+            name: 'Disable ALL plugins (remembering active ones)',
+            callback: async () => {
+                // @ts-ignore
+                const enabledPlugins = Object.keys(this.app.plugins.plugins);
+                this.settings.lastActivePlugins = enabledPlugins.filter(p => p !== this.manifest.id);
+                await this.saveSettings();
+
+                for (const pluginId of enabledPlugins) {
+                    if (pluginId === this.manifest.id && !this.settings.disableSelf) {
+                        continue;
+                    }
+                    // @ts-ignore
+                    await this.app.plugins.disablePlugin(pluginId);
+                }
+            }
+        });
+
+        this.addCommand({
+            id: 'plugins-toggler-reenable-remembered',
+            name: 'Re-enable remembered plugins',
+            callback: async () => {
+                const pluginsToEnable = [...this.settings.lastActivePlugins];
+                this.settings.lastActivePlugins = [];
+                await this.saveSettings();
+
+                for (const pluginId of pluginsToEnable) {
+                    // @ts-ignore
+                    await this.app.plugins.enablePlugin(pluginId);
+                }
+            }
+        });
+    }
+
+    onunload() {
+        // Obsidian automatically unregisters all commands on unload.
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+}
+
+class PluginsTogglerSettingTab extends PluginSettingTab {
+    plugin: PluginsToggler;
+
+    constructor(app: App, plugin: PluginsToggler) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
+
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
+
+        new Setting(containerEl)
+            .setName('Disable self with ALL commands')
+            .setDesc('If enabled, this plugin will also be disabled when using \'Disable ALL plugins\' commands.')
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.disableSelf)
+                .onChange(async (value) => {
+                    this.plugin.settings.disableSelf = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
+}
